e8c66d591b787062dbfee4b5397d82f6420b7745
Dylan Moore <moore.dylan.e@gmail.com>
2015-01-31 20:18:56 -0800

    Trim username input

--- client/username_form.js
+++ client/username_form.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
   Template.userForm.events({
     'submit .user-input': function (event) {
-      var userName = document.getElementById('user-name').value;
+      var userName = document.getElementById('user-name').value.trim();
 
       Session.set('userName', userName);
       Session.set('commentsListUsed', true);
